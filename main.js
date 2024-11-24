930a47fad26f9750f7ea528d4be1ac1574ea035a
Salahitdinov2012 <salahitdinovmuhammadamin@gmail.com>
2024-11-24 20:52:41 +0000

    qwerty

---
+++ main.js
@@ -0,0 +1,64 @@
+// let b=23
+// let c=10
+// let d=20
+// let a=b+c*d
+// console.log("Javob",a);
+
+// let a=30
+// let b=32
+// let c=10
+// let d=40
+// let m=(a-b)*(c-d)
+// console.log("Javob",m);
+
+// let a=78
+// let b=4
+// let c=49
+// let d=7
+// let k=a+b*c/d
+// console.log("Javob",k)
+
+// let a=9
+// let b=20
+// let c=2
+// let d=a/(b-c)
+// console.log("Javob",d)
+
+// let a=0.5
+// let b=22
+// let d=67
+// let c=a*a/d*(a-b)
+// console.log("Javob",c)
+
+// let c=75
+// let m=43
+// let d=9
+// let n=56
+// let a=m*n+c/d
+// console.log("Javob",a)
+
+// let b=68
+// let c=89
+// let d=62
+// let a=d-(b+d)/c 
+// console.log("Javob",a)
+
+// let b=78
+// let c=89
+// let a=b-(c-b)
+// console.log("Javob",a)
+
+// let b=80
+// let a=b-(a+b)/(a-b)
+// console.log("Javob",a)
+
+// let b=43
+// let c=67
+// let d=98
+// let e=57
+// let a=(c-b)/(c+d)*(d-e)
+// console.log("Javob",a)
+
+
+
+
